b845edf9022f01a105081a815407de7db094b47b
dahuget <danaliyahuget@gmail.com>
2018-10-03 21:38:08 -0700

    implement map problem and print tree solutions

--- src/ProblemMap.java
+++ src/ProblemMap.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class ProblemMap extends Problem {
 	Map<String, Map<String, Double>> map;
@@ -72,14 +74,57 @@ public class ProblemMap extends Problem {
 	}
 
 	public static void main(String[] args) throws Exception {
-		ProblemMap problem = new ProblemMap("romania.txt","romaniaSLD.txt");
+		ProblemMap problem = new ProblemMap("/Users/danahuget/Documents/CSC421/AI-Search/romania.txt","/Users/danahuget/Documents/CSC421/AI-Search/romaniaSLD.txt");
 		problem.initialState = "Timisoara";
 		problem.goalState = "Bucharest";
 		
 		Search search  = new Search(problem);
-		
-		System.out.println("BreadthFirstTreeSearch:\t\t" + search.BreadthFirstTreeSearch());
-		
-		System.out.println("BreadthFirstGraphSearch:\t" + search.BreadthFirstGraphSearch());
+		/*
+		  QUESTION 1:
+		• Breadth-first search (BFS) provided
+		• Uniform-cost search (UCS) provided
+		• Depth-first search (DFS) provided
+		• Depth-limited search (DLS) implemented
+		• Iterative deepening search (IDS) implemented
+		 */
+		Path file = Paths.get("ProblemMapStatisticsReport.txt");
+		List<String> stats = new ArrayList<>();
+		stats.add("Q1 Statistics:");
+//		System.out.println("BreadthFirstTreeSearch:\t\t\t" + search.BreadthFirstTreeSearch());
+		stats.add("BreadthFirstTreeSearch:\t\t" + search.BreadthFirstTreeSearch());
+//		System.out.println("BreadthFirstGraphSearch:\t\t" + search.BreadthFirstGraphSearch());
+		stats.add("BreadthFirstGraphSearch:\t" + search.BreadthFirstGraphSearch());
+//		System.out.println("UniformCostTreeSearch:\t\t\t" + search.UniformCostTreeSearch());
+		stats.add("UniformCostTreeSearch:\t\t" + search.UniformCostTreeSearch());
+//		System.out.println("UniformCostGraphSearch:\t\t\t" + search.UniformCostGraphSearch());
+		stats.add("UniformCostGraphSearch:\t\t" + search.UniformCostGraphSearch());
+//		System.out.println("DepthFirstTreeSearch:\t\t\t" + search.DepthFirstTreeSearch());
+		stats.add("DepthFirstTreeSearch:\t\t" + search.DepthFirstTreeSearch());
+//		System.out.println("DepthFirstGraphSearch:\t\t\t" + search.DepthFirstGraphSearch());
+		stats.add("DepthFirstGraphSearch:\t\t" + search.DepthFirstGraphSearch());
+//		System.out.println("TreeSearchDepthLimited(4):\t\t" + search.TreeSearchDepthLimited(4));
+		stats.add("TreeSearchDepthLimited(4):\t" + search.TreeSearchDepthLimited(4));
+//		System.out.println("GraphSearchDepthLimited(4):\t\t" + search.GraphSearchDepthLimited(4));
+		stats.add("GraphSearchDepthLimited(4):\t" + search.GraphSearchDepthLimited(4));
+//		System.out.println("IterativeDeepeningTreeSearch:\t" + search.IterativeDeepeningTreeSearch());
+		stats.add("IterativeDeepeningTreeSearch:\t" + search.IterativeDeepeningTreeSearch());
+//		System.out.println("IterativeDeepeningGraphSearch:\t" + search.IterativeDeepeningGraphSearch());
+		stats.add("IterativeDeepeningGraphSearch:\t" + search.IterativeDeepeningGraphSearch());
+
+		// QUESTION 2:
+		stats.add("\nQ2 Statistics:");
+//		System.out.println("AstarTreeSearch:\t\t\t\t" + search.AstarTreeSearch());
+		stats.add("AstarTreeSearch:\t\t" + search.AstarTreeSearch());
+		search.PrintTree("FinalAstarTreeSearchTree.txt");
+
+//		System.out.println("AstarGraphSearch:\t\t\t\t" + search.AstarGraphSearch());
+		stats.add("AstarGraphSearch:\t\t" + search.AstarGraphSearch());
+		search.PrintTree("FinalAstarGraphSearchTree.txt");
+
+		try {
+			Files.write(file, stats, Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}	
 }
--- src/Search.java
+++ src/Search.java
@@ -1,9 +1,9 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Search {
 	Problem problem;
@@ -30,6 +30,11 @@ public class Search {
 	public String AstarTreeSearch() {
 		return TreeSearch(new FrontierPriorityQueue(new ComparatorF(problem)));
 	}
+
+	public String TreeSearchDepthLimited(int limit) {
+		cnt = 0;
+		return TreeSearchDepthLimited(new FrontierLIFO(), limit);
+	}
 	
 	//Graph-search methods
 	public String BreadthFirstGraphSearch() {
@@ -52,15 +57,55 @@ public class Search {
 		return GraphSearch(new FrontierPriorityQueue(new ComparatorF(problem)));
 	}
 
+	public String GraphSearchDepthLimited(int limit) {
+		cnt = 0;
+		return GraphSearchDepthLimited(new FrontierLIFO(), limit);
+	}
+
+	/*
+	QUESTION 1
+	• Breadth-first search (BFS) done
+	• Uniform-cost search (UCS) done
+	• Depth-first search (DFS) done
+	• Depth-limited search (DLS) impl
+	• Iterative deepening search (IDS) impl
+	 */
 	
-	//Iterative deepening, tree-search and graph-search
+	//Iterative deepening, tree-search and graph-search QUESTION 1
+	// Depth-limited search (DFS, LIFO Frontier)
+	/*
+	function IterativeDeepening-TREE-SEARCH(problem) returns a solution, or failure
+		for limit=0 to infinity
+			result = DepthLimited-TREE-SEARCH(problem, limit)
+			if result is a solution return result
+	 */
 	public String IterativeDeepeningTreeSearch() {
 		//TODO
+		cnt = 0;
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			String result = TreeSearchDepthLimited(new FrontierLIFO(), i);
+			if (result != null)
+				return result;
+		}
+		// not reached because above will loop until solution is found
 		return null;
 	}
-	
+
+	/*
+	function IterativeDeepening-GRAPH-SEARCH(problem) returns a solution, or failure
+	for limit=0 to infinity
+		result = DepthLimited-GRAPH-SEARCH(problem, limit)
+		if result is a solution return result
+	 */
 	public String IterativeDeepeningGraphSearch() {
 		//TODO
+		cnt = 0;
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			String result = GraphSearchDepthLimited(new FrontierLIFO(), i);
+			if (result != null)
+				return result;
+		}
+		// not reached because above will loop until solution is found
 		return null;	
 	}
 	
@@ -94,12 +139,12 @@ public class Search {
 	}
 
 	private String GraphSearch(Frontier frontier) {
-		cnt = 0; 
+		cnt = 0;
 		node_list = new ArrayList<Node>();
-		
-		initialNode = MakeNode(problem.initialState); 
+
+		initialNode = MakeNode(problem.initialState);
 		node_list.add( initialNode );
-		
+
 		Set<Object> explored = new HashSet<Object>(); //empty set
 		frontier.insert( initialNode );
 		while(true) {
@@ -119,15 +164,78 @@ public class Search {
 			}
 		}
 	}
-	
+
+	/*
+	function DepthLimited-TREE-SEARCH(problem, limit) returns a solution, or failure
+		initialize the frontier using the initial state of problem
+		loop do
+			if the frontier is empty then return failure
+			remove n from the frontier
+			if n contains a goal state then return the corresponding solution
+			if the depth of n is less than limit then
+				expand n adding the resulting nodes to the frontier
+	 */
+	// QUESTION 1
 	private String TreeSearchDepthLimited(Frontier frontier, int limit) {
 		//TODO
-		return null;
+		node_list = new ArrayList<Node>();
+
+		initialNode = MakeNode(problem.initialState);
+		node_list.add( initialNode );
+
+		frontier.insert( initialNode );
+		while(true) {
+			if (frontier.isEmpty())
+				return null;
+
+			Node node = frontier.remove();
+			if( problem.goal_test(node.state) )
+				return Solution(node);
+
+			if (node.depth < limit) {
+				frontier.insertAll(Expand(node, problem));
+				cnt++;
+			}
+
+		}
 	}
+
+	/*
+	function DepthLimited-GRAPH-SEARCH(problem, limit) returns a solution, or failure
+		initialize the frontier using the initial state of problem
+		initialize the explored set to be empty
+		loop do
+			if the frontier is empty then return failure
+			remove n from the frontier
+			if n contains a goal state then return the corresponding solution
+			if the state of n is not in explored and the depth of n is less than limit
+				add the state of n to explored
+				expand n adding the resulting nodes to the frontier
+	 */
 
 	private String GraphSearchDepthLimited(Frontier frontier, int limit) {
 		//TODO
-		return null;	
+		node_list = new ArrayList<Node>();
+
+		initialNode = MakeNode(problem.initialState);
+		node_list.add( initialNode );
+
+		Set<Object> explored = new HashSet<Object>(); //empty set
+		frontier.insert( initialNode );
+		while(true) {
+			if (frontier.isEmpty())
+				return null;
+
+			Node node = frontier.remove();
+			if( problem.goal_test(node.state) )
+				return Solution(node);
+
+			if( !explored.contains(node.state) && node.depth < limit) {
+				explored.add(node.state);
+				frontier.insertAll(Expand(node, problem));
+				cnt++;
+			}
+		}
 	}
 
 	private Node MakeNode(Object state) {
@@ -174,5 +282,45 @@ public class Search {
 			solution_str += solution.pop() + " ";
 		
 		return solution_str;
+	}
+
+	/* QUESTION 2
+	print out the final search tree, and add at each node its path cost value (g), the heuristic
+	function value (h), their sum (f=g+h), and the order of expansion, e.g. the root will be 0,
+	then the next node expanded will be 1, etc.
+	Timisoara(g=0.0, h=329.0, f=329.0) order=0
+		Arad(g=118.0, h=366.0, f=484.0) order=3
+			...
+		Lugoj(g=111.0, h=244.0, f=355.0) order=1
+			...
+	 */
+
+	List<String> tree = new ArrayList<>();
+
+	public void PrintTree(String fileName) {
+		PrintTree(initialNode);
+		Path file = Paths.get(fileName);
+		try {
+			Files.write(file, tree, Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void PrintTree(Node n) {
+		tree.add(getNodeString(n));
+		for (Node m: node_list) {
+			if (m.parent_node == n)
+				PrintTree(m);
+		}
+	}
+
+	private String getNodeString(Node n) {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < n.depth; i++) {
+			s.append("  ");
+		}
+		s.append(n.state+"(g="+n.path_cost+", h="+problem.h(n.state)+", f="+(n.path_cost+problem.h(n.state))+") order="+n.order);
+		return s.toString();
 	}
 }
